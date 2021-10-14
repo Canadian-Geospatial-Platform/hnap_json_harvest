152333632c513152e05f06205bc2c9ca5473a97c
Bo Lu <bo.lu@canada.ca>
2021-10-14 11:52:29 -0400

    upload_file function isn't used, it has been removed

--- hnap_json_harvest/app.py
+++ hnap_json_harvest/app.py
@@ -371,28 +371,6 @@ def create_bucket(bucket_name, region=None):
             return False
         return True #Success
 
-def upload_file(file_name, bucket, object_name=None):
-    """Upload a file to an S3 bucket
-
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
-
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = file_name
-
-    # Upload the file
-    s3_client = boto3.client('s3')
-    try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
-
 def upload_json_file(file_name, bucket, json_data, object_name=None):
     """Upload a json file to an S3 bucket
 
